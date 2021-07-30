84cc538fae5544cc66f0f98b7adbecbbdb27d5e2
Serap Pala <72605128+SerapPala@users.noreply.github.com>
2021-07-30 12:03:31 +0300

    Add files via upload

---
+++ odev/src/app/topbar/topbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+@Component({
+  selector: 'app-topbar',
+  templateUrl: './topbar.component.html',
+  styleUrls: ['./topbar.component.scss']
+})
+export class TopbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
