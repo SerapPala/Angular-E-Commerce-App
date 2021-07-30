982facf7bf5dae99ae385d4cc06262cb6bf825f5
Serap Pala <72605128+SerapPala@users.noreply.github.com>
2021-07-30 12:03:51 +0300

    Add files via upload

---
+++ odev/src/app/trend/trend.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+@Component({
+  selector: 'app-trend',
+  templateUrl: './trend.component.html',
+  styleUrls: ['./trend.component.scss']
+})
+export class TrendComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    navSpeed: 700,
+    navText: ['<<', '>>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 1
+      },
+      740: {
+        items: 1
+      },
+      940: {
+        items: 4
+      }
+    },
+    nav: true
+  }
+}
