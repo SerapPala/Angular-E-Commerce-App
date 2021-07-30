f4707c151e3030eebfce4a59913193e220b1080f
Serap Pala <72605128+SerapPala@users.noreply.github.com>
2021-07-30 11:59:04 +0300

    Add files via upload

---
+++ odev/src/app/header/header.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../services/account.service';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+
+  constructor(private accountService:AccountService){}
+  title = 'shop';
+  isLoggedin(){
+    return this.accountService.isLoggedIn();
+  }
+  logOut(){
+    this.accountService.logOut();
+  }
+
+  ngOnInit(): void {
+  }
+
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    navSpeed: 700,
+    navText: ['<<', '>>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 1
+      },
+      740: {
+        items: 1
+      },
+      940: {
+        items: 1
+      }
+    },
+    nav: true
+  }
+}
